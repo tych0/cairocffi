60643d23040e7251114a61749a495608b3d9d663
Sean Vig <sean.v.775@gmail.com>
2014-08-15 07:22:29 -0500

    module

--- cairocffi/xcb.py
+++ cairocffi/xcb.py
@@ -19,6 +19,9 @@ ffi.include(cairo_ffi)
 ffi.include(xcb_ffi)
 ffi.cdef(constants._CAIRO_XCB_HEADERS)
 
+import weakref
+global_weakkeydict = weakref.WeakKeyDictionary()
+
 cairo_xcb = dlopen(ffi, 'libcairo.so.2', 'libcairo.2.dylib', 'libcairo-2.dll',
                    'cairo', 'libcairo-2')
 
@@ -47,7 +50,6 @@ class XCBSurface(Surface):
 
         p = cairo_xcb.cairo_xcb_surface_create(
             conn._conn, drawable, c_visual, width, height)
-        global_weakkeydict = weakref.WeakKeyDictionary()
         global_weakkeydict[p] = c_visual
         Surface.__init__(self, p)
 
